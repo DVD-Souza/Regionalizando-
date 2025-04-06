d785bc2f150faeb65815b50b8d8a3cb29925956d
Deivid <deivid@debian.com>
2025-04-05 22:26:13 -0300

    correction from previous step v2

---
+++ backend/model/User.js
@@ -0,0 +1,15 @@
+class User {
+    #id;
+    #name;
+    #email;
+    #password;
+    
+    constructor(id, name, email, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password
+    }
+
+    
+}
